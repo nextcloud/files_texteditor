d5d2187479fb45ee863bbc8472ec9585395b995d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-25 02:11:12 -0500

    [tx-robot] updated from transifex

---
+++ l10n/az.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "Cannot save file as it has been modified since opening" : "Faylı saxlamaq mümkün deyil cünki, açılma müddətində dəyişdirilib",
+    "Insufficient permissions" : "Qeyri kafi yetkilər",
+    "File path not supplied" : "Fayl ünvanı təqdim edilmir",
+    "File mtime not supplied" : "Faylda dəyişmə vaxtı göstərilmir",
+    "Save" : "Saxlamaq",
+    "Search" : "Axtarış",
+    "Next" : "Növbəti",
+    "Clear" : "Təmizlə",
+    "Saving..." : "Saxlama...",
+    "An error occurred!" : "Səhv baş verdi!",
+    "There are unsaved changes in the text editor" : "Tekst redaktə edənin içində, yadda saxlanılmamış dəyişikliklər mövcuddur",
+    "There were unsaved changes, click here to go back" : "Qeyd edilməyən dəyişikliklər var idi, geri qayıtmaq üçün buraya sıxın"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/az.php
@@ -0,0 +1,16 @@
+<?php
+$TRANSLATIONS = array(
+"Cannot save file as it has been modified since opening" => "Faylı saxlamaq mümkün deyil cünki, açılma müddətində dəyişdirilib",
+"Insufficient permissions" => "Qeyri kafi yetkilər",
+"File path not supplied" => "Fayl ünvanı təqdim edilmir",
+"File mtime not supplied" => "Faylda dəyişmə vaxtı göstərilmir",
+"Save" => "Saxlamaq",
+"Search" => "Axtarış",
+"Next" => "Növbəti",
+"Clear" => "Təmizlə",
+"Saving..." => "Saxlama...",
+"An error occurred!" => "Səhv baş verdi!",
+"There are unsaved changes in the text editor" => "Tekst redaktə edənin içində, yadda saxlanılmamış dəyişikliklər mövcuddur",
+"There were unsaved changes, click here to go back" => "Qeyd edilməyən dəyişikliklər var idi, geri qayıtmaq üçün buraya sıxın"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
