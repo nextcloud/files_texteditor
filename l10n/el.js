65a97911ce664cc7dfb1bda725448f67c696e31b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-28 02:11:12 -0500

    [tx-robot] updated from transifex

---
+++ l10n/el.js
@@ -0,0 +1,14 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "Insufficient permissions" : "Μη επαρκή δικαιώματα",
+    "Save" : "Αποθήκευση",
+    "Search" : "Αναζήτηση",
+    "Next" : "Επόμενο",
+    "Clear" : "Εκκαθάριση",
+    "Saving..." : "Γίνεται αποθήκευση...",
+    "An error occurred!" : "Παρουσιάστηκε σφάλμα!",
+    "There are unsaved changes in the text editor" : "Υπάρχουν μη αποθηκευμένες αλλαγές στον επεξεργαστή κειμένου",
+    "There were unsaved changes, click here to go back" : "Υπάρχουν μη αποθηκευμένες αλλαγές, κάντε κλικ εδώ για να επιστρέψετε πίσω"
+},
+"nplurals=2; plural=(n != 1);");
--- l10n/el.php
+++ l10n/el.php
@@ -1,12 +1,13 @@
-<?php $TRANSLATIONS = array(
+<?php
+$TRANSLATIONS = array(
+"Insufficient permissions" => "Μη επαρκή δικαιώματα",
 "Save" => "Αποθήκευση",
 "Search" => "Αναζήτηση",
-"Close" => "Κλείσιμο",
 "Next" => "Επόμενο",
 "Clear" => "Εκκαθάριση",
 "Saving..." => "Γίνεται αποθήκευση...",
-"Failed to save file" => "Αποτυχία αποθήκευσης αρχείου",
 "An error occurred!" => "Παρουσιάστηκε σφάλμα!",
 "There are unsaved changes in the text editor" => "Υπάρχουν μη αποθηκευμένες αλλαγές στον επεξεργαστή κειμένου",
 "There were unsaved changes, click here to go back" => "Υπάρχουν μη αποθηκευμένες αλλαγές, κάντε κλικ εδώ για να επιστρέψετε πίσω"
 );
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
