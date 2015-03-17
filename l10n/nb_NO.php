28c964409a7152a60d15475962ce4d89db19a6bd
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-03-17 02:11:37 -0400

    [tx-robot] updated from transifex

--- l10n/nb_NO.php
+++ l10n/nb_NO.php
@@ -1,11 +1,16 @@
-<?php $TRANSLATIONS = array(
+<?php
+$TRANSLATIONS = array(
+"Cannot save file as it has been modified since opening" => "Kan ikke lagre filen siden den har blitt endret.",
+"Insufficient permissions" => "Ikke rettigheter",
+"File path not supplied" => "Filbane ikke angitt",
+"File mtime not supplied" => "Filens mtime ikke angitt",
 "Save" => "Lagre",
 "Search" => "Søk",
-"Close" => "Lukk",
 "Next" => "Neste",
 "Clear" => "Tøm",
 "Saving..." => "Lagrer...",
-"Failed to save file" => "Kunne ikke lagre filen",
 "An error occurred!" => "En feil oppstod!",
+"There are unsaved changes in the text editor" => "Det finnes endringer i tekst-editoren som ikke er lagret",
 "There were unsaved changes, click here to go back" => "Det var ulagrede endringer, klikk her for å gå tilbake"
 );
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
