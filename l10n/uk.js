19eeb569678e50143e05d86cd8b3232ebc20a3c9
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-03-19 02:11:37 -0400

    [tx-robot] updated from transifex

---
+++ l10n/uk.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "Cannot save file as it has been modified since opening" : "Неможливо зберегти файл оскільки він був змінений після відкриття",
+    "Insufficient permissions" : "Недостатньо прав доступу",
+    "File path not supplied" : "Невідомий шлях файлу",
+    "File mtime not supplied" : "Невідомий час модифікації файлу",
+    "Save" : "Зберегти",
+    "Search" : "Пошук",
+    "Next" : "Наступний",
+    "Clear" : "Очистити",
+    "Saving..." : "Зберігаю...",
+    "An error occurred!" : "Сталася помилка!",
+    "There are unsaved changes in the text editor" : "Є незбережені зміни в текстовому редакторі",
+    "There were unsaved changes, click here to go back" : "Були незбережені зміни, натисніть сюди для повернення"
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
--- l10n/uk.php
+++ l10n/uk.php
@@ -1,12 +1,16 @@
-<?php $TRANSLATIONS = array(
+<?php
+$TRANSLATIONS = array(
+"Cannot save file as it has been modified since opening" => "Неможливо зберегти файл оскільки він був змінений після відкриття",
+"Insufficient permissions" => "Недостатньо прав доступу",
+"File path not supplied" => "Невідомий шлях файлу",
+"File mtime not supplied" => "Невідомий час модифікації файлу",
 "Save" => "Зберегти",
 "Search" => "Пошук",
-"Close" => "Закрити",
 "Next" => "Наступний",
 "Clear" => "Очистити",
 "Saving..." => "Зберігаю...",
-"Failed to save file" => "Неможливо зберегти файл",
 "An error occurred!" => "Сталася помилка!",
 "There are unsaved changes in the text editor" => "Є незбережені зміни в текстовому редакторі",
 "There were unsaved changes, click here to go back" => "Були незбережені зміни, натисніть сюди для повернення"
 );
+$PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
