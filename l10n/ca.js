fbee68ad5b668c9e51adbe58b53dcd36dd79ed47
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-04-09 02:11:14 -0400

    [tx-robot] updated from transifex

---
+++ l10n/ca.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "Cannot save file as it has been modified since opening" : "No és possible guardar l'arxiu perquè ha estat modificat després de obrir-se",
+    "Insufficient permissions" : "Permisos insuficients",
+    "File path not supplied" : "No s'ha proporcionat la ruta del arxiu",
+    "File mtime not supplied" : "No s'ha proporcionat l'hora de modificació de l'arxiu",
+    "Save" : "Desa",
+    "Search" : "Cerca",
+    "Next" : "Següent",
+    "Clear" : "Neteja",
+    "Saving..." : "Desant...",
+    "An error occurred!" : "S'ha produït un error!",
+    "There are unsaved changes in the text editor" : "Hi ha canvis no desats a l'editor de text",
+    "There were unsaved changes, click here to go back" : "Hi havia canvis que no desats, feu click aquí per anar enrera"
+},
+"nplurals=2; plural=(n != 1);");
--- l10n/ca.php
+++ l10n/ca.php
@@ -1,12 +1,16 @@
-<?php $TRANSLATIONS = array(
+<?php
+$TRANSLATIONS = array(
+"Cannot save file as it has been modified since opening" => "No és possible guardar l'arxiu perquè ha estat modificat després de obrir-se",
+"Insufficient permissions" => "Permisos insuficients",
+"File path not supplied" => "No s'ha proporcionat la ruta del arxiu",
+"File mtime not supplied" => "No s'ha proporcionat l'hora de modificació de l'arxiu",
 "Save" => "Desa",
 "Search" => "Cerca",
-"Close" => "Tanca",
 "Next" => "Següent",
 "Clear" => "Neteja",
 "Saving..." => "Desant...",
-"Failed to save file" => "Ha fallat en desar el fitxer",
 "An error occurred!" => "S'ha produït un error!",
 "There are unsaved changes in the text editor" => "Hi ha canvis no desats a l'editor de text",
 "There were unsaved changes, click here to go back" => "Hi havia canvis que no desats, feu click aquí per anar enrera"
 );
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
