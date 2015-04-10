9e7fabdb619dbc6397ab02fa5eb5f9062fb1d315
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-04-10 02:11:13 -0400

    [tx-robot] updated from transifex

---
+++ l10n/sr@latin.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "Cannot save file as it has been modified since opening" : "Ne mogu da sačuvam fajl jer je izmenjen otkako je otvoren",
+    "Insufficient permissions" : "Nedovoljne dozvole",
+    "File path not supplied" : "Nije dobijena putanja fajla",
+    "File mtime not supplied" : "Nije dobijeno vreme izmene fajla",
+    "Save" : "Sačuvaj",
+    "Search" : "Traži",
+    "Next" : "Sledeće",
+    "Clear" : "Očisti",
+    "Saving..." : "Upisujem...",
+    "An error occurred!" : "Došlo je do greške!",
+    "There are unsaved changes in the text editor" : "Ima nesačuvanih izmena u uređivaču teksta",
+    "There were unsaved changes, click here to go back" : "Bilo je nesačuvanih izmena. Kliknite ovde da se vratite."
+},
+"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
