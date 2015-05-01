69a61197dbef45e99d1356873b49df4077290050
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-05-01 02:11:13 -0400

    [tx-robot] updated from transifex

---
+++ l10n/id.js
@@ -0,0 +1,17 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "Cannot save file as it has been modified since opening" : "Tidak dapat menyimpan berkas karena sudah diubah sejak dibuka",
+    "Insufficient permissions" : "Izin tidak cukup",
+    "File path not supplied" : "Lokasi berkas belum diberikan",
+    "File mtime not supplied" : "Berkas mtime belum diberikan",
+    "Save" : "Simpan",
+    "Search" : "Cari",
+    "Next" : "Selanjutnya",
+    "Clear" : "Bersihkan",
+    "Saving..." : "Menyimpan...",
+    "An error occurred!" : "Terjadi kesalahan!",
+    "There are unsaved changes in the text editor" : "Ada perubahan yang belum disimpan pada penyunting teks",
+    "There were unsaved changes, click here to go back" : "Ada perubahan yang belum tersimpan, klik di sini untuk kembali"
+},
+"nplurals=1; plural=0;");
--- l10n/id.php
+++ l10n/id.php
@@ -1,12 +1,16 @@
-<?php $TRANSLATIONS = array(
+<?php
+$TRANSLATIONS = array(
+"Cannot save file as it has been modified since opening" => "Tidak dapat menyimpan berkas karena sudah diubah sejak dibuka",
+"Insufficient permissions" => "Izin tidak cukup",
+"File path not supplied" => "Lokasi berkas belum diberikan",
+"File mtime not supplied" => "Berkas mtime belum diberikan",
 "Save" => "Simpan",
 "Search" => "Cari",
-"Close" => "Tutup",
 "Next" => "Selanjutnya",
 "Clear" => "Bersihkan",
 "Saving..." => "Menyimpan...",
-"Failed to save file" => "Gagal menyimpan berkas",
 "An error occurred!" => "Terjadi kesalahan!",
 "There are unsaved changes in the text editor" => "Ada perubahan yang belum disimpan pada penyunting teks",
 "There were unsaved changes, click here to go back" => "Ada perubahan yang belum tersimpan, klik di sini untuk kembali"
 );
+$PLURAL_FORMS = "nplurals=1; plural=0;";
