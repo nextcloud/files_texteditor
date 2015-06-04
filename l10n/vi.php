a568afe0c22060f1d9708ce533526ada4ed45ee1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-04 02:11:14 -0400

    [tx-robot] updated from transifex

--- l10n/vi.php
+++ l10n/vi.php
@@ -1,12 +1,13 @@
-<?php $TRANSLATIONS = array(
+<?php
+$TRANSLATIONS = array(
+"Insufficient permissions" => "Không đủ quyền",
 "Save" => "Lưu",
 "Search" => "Tìm kiếm",
-"Close" => "Đóng",
 "Next" => "Kế tiếp",
 "Clear" => "Xóa",
 "Saving..." => "Đang lưu...",
-"Failed to save file" => "Lỗi khi lưu file",
 "An error occurred!" => "Có một lỗi đã xảy ra!",
 "There are unsaved changes in the text editor" => "Có những thay đổi bạn chưa lưu trong trình soạn thảo ",
 "There were unsaved changes, click here to go back" => "Thay đổi không được lưu, bấm vào đây để trở lại"
 );
+$PLURAL_FORMS = "nplurals=1; plural=0;";
