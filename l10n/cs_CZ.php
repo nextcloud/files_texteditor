56d5942bba30bedc17e88b76d5e0eaba3aa9c7dd
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-28 02:11:14 -0400

    [tx-robot] updated from transifex

--- l10n/cs_CZ.php
+++ l10n/cs_CZ.php
@@ -1,6 +1,7 @@
 <?php
 $TRANSLATIONS = array(
 "Can not read the file." => "Soubor nelze přečíst.",
+"Invalid file path supplied." => "Zadána neplatná cesta k souboru.",
 "Cannot save file as it has been modified since opening" => "Soubor nelze uložit, protože byl od otevření upraven",
 "Insufficient permissions" => "Nedostatečná oprávnění",
 "File path not supplied" => "Nebyla dodána cesta k souboru",
