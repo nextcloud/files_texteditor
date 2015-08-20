1d9130c56bd91910b64c792c31bf22987202da8f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-08-20 02:11:19 -0400

    [tx-robot] updated from transifex

--- l10n/ja.php
+++ l10n/ja.php
@@ -1,6 +1,7 @@
 <?php
 $TRANSLATIONS = array(
 "Can not read the file." => "ファイルを読み込めません",
+"Invalid file path supplied." => "無効なファイルパスが提供されました。",
 "Cannot save file as it has been modified since opening" => "変更したファイルは使用中のため保存できません",
 "Insufficient permissions" => "権限が不足しています",
 "File path not supplied" => "ファイルパスが足りません",
