d4cf0af5f10e67469aa87453b76f5c69da87d563
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-09-08 02:11:14 -0400

    [tx-robot] updated from transifex

--- l10n/pt_PT.js
+++ l10n/pt_PT.js
@@ -1,17 +1,17 @@
 OC.L10N.register(
     "files_texteditor",
     {
-    "Cannot save file as it has been modified since opening" : "Não é possível salvar o ficheiro como ele foi modificado desde a abertura",
+    "Cannot read the file." : "Não é possível ler o ficheiro.",
+    "Invalid file path supplied." : "Caminho do ficheiro indicado inválido.",
+    "Cannot save file as it has been modified since opening" : "Não é possível guardar o ficheiro porque este foi modificado desde a abertura",
     "Insufficient permissions" : "Permissões insuficientes",
-    "File path not supplied" : "Nenhum caminho do ficheiro foi fornecido",
-    "File mtime not supplied" : "Ficheiro mtime não foi fornecido",
-    "Save" : "Guardar",
-    "Search" : "Procurar",
-    "Next" : "Próximo",
-    "Clear" : "Limpar.",
-    "Saving..." : "A guardar...",
-    "An error occurred!" : "Ocorreu um erro!",
-    "There are unsaved changes in the text editor" : "Existem alterações no editor de texto que ainda não foram guardadas",
-    "There were unsaved changes, click here to go back" : "Existiram alterações que não foram guardadas, Clique aqui para voltar atrás"
+    "File path not supplied" : "Caminho do ficheiro não indicado",
+    "File mtime not supplied" : "Ficheiro mtime não foi indicado",
+    "saving..." : "a guardar...",
+    "saved!" : "guardado!",
+    "failed!" : "falhou!",
+    "Saved" : "Guardado",
+    "There was a problem saving your changes. Click to resume editing." : "Ocorreu um problema ao guardar as suas alterações. Clique para retomar a edição.",
+    "An error occurred!" : "Ocorreu um erro!"
 },
 "nplurals=2; plural=(n != 1);");
