f6308ff26dd6ffd07e4f7ceea5d8d2938f5ff847
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-09-12 02:11:17 -0400

    [tx-robot] updated from transifex

--- l10n/es.php
+++ l10n/es.php
@@ -1,17 +1,16 @@
 <?php
 $TRANSLATIONS = array(
+"Cannot read the file." => "No se puede leer el archivo.",
 "Invalid file path supplied." => "Ruta de archivo inválida.",
 "Cannot save file as it has been modified since opening" => "No es posible guardar el archivo; pues ha sido modificado",
 "Insufficient permissions" => "Permisos insuficientes",
 "File path not supplied" => "No se ha proporcionado la ruta del archivo",
 "File mtime not supplied" => "No se ha proporcionado la hora de modificación del archivo (mtime)",
-"Save" => "Guardar",
-"Search" => "Buscar",
-"Next" => "Siguiente",
-"Clear" => "Borrar",
-"Saving..." => "Guardando...",
-"An error occurred!" => "¡Ocurrió un error!",
-"There are unsaved changes in the text editor" => "Hay cambios sin guardar en el editor de textos",
-"There were unsaved changes, click here to go back" => "Hay cambios sin guardar, haga clic aquí para volver"
+"saving..." => "guardando....",
+"saved!" => "¡guardado!",
+"failed!" => "¡falló!",
+"Saved" => "Guardado",
+"There was a problem saving your changes. Click to resume editing." => "Hubo un problema al guardar los cambios. Haga clic para reanudar la edición.",
+"An error occurred!" => "¡Ocurrió un error!"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- l10n/ko.php
+++ l10n/ko.php
@@ -1,17 +1,16 @@
 <?php
 $TRANSLATIONS = array(
+"Cannot read the file." => "파일을 읽을 수 없습니다.",
 "Invalid file path supplied." => "잘못된 경로를 지정하였습니다.",
 "Cannot save file as it has been modified since opening" => "파일을 연 후 수정했기 때문에 저장할 수 없음",
 "Insufficient permissions" => "권한 부족",
 "File path not supplied" => "파일 경로가 지정되지 않았음",
 "File mtime not supplied" => "파일 수정 시간이 지정되지 않았음",
-"Save" => "저장",
-"Search" => "검색",
-"Next" => "다음",
-"Clear" => "지우기",
-"Saving..." => "저장하는 중...",
-"An error occurred!" => "오류가 발생하였습니다!",
-"There are unsaved changes in the text editor" => "저장되지 않은 변경 사항이 있습니다.",
-"There were unsaved changes, click here to go back" => "저장되지 않은 변경 사항이 있습니다. 여기를 누르면 돌아갑니다"
+"saving..." => "저장 중...",
+"saved!" => "저장했습니다!",
+"failed!" => "실패했습니다!",
+"Saved" => "저장됨",
+"There was a problem saving your changes. Click to resume editing." => "변경 사항을 저장할 수 없습니다. 편집을 계속하려면 누르십시오.",
+"An error occurred!" => "오류가 발생하였습니다!"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
