1f180e3e98b3b9c2430b3bed7ccf119d5b6f2683
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-09-14 02:11:17 -0400

    [tx-robot] updated from transifex

--- l10n/nb_NO.php
+++ l10n/nb_NO.php
@@ -1,17 +1,16 @@
 <?php
 $TRANSLATIONS = array(
+"Cannot read the file." => "Kan ikke lese filen.",
 "Invalid file path supplied." => "Ugyldig sti angitt.",
 "Cannot save file as it has been modified since opening" => "Kan ikke lagre filen siden den har blitt endret.",
 "Insufficient permissions" => "Ikke rettigheter",
 "File path not supplied" => "Filbane ikke angitt",
 "File mtime not supplied" => "Filens mtime ikke angitt",
-"Save" => "Lagre",
-"Search" => "Søk",
-"Next" => "Neste",
-"Clear" => "Tøm",
-"Saving..." => "Lagrer...",
-"An error occurred!" => "En feil oppstod!",
-"There are unsaved changes in the text editor" => "Det finnes endringer i tekst-editoren som ikke er lagret",
-"There were unsaved changes, click here to go back" => "Det var ulagrede endringer, klikk her for å gå tilbake"
+"saving..." => "lagrer...",
+"saved!" => "lagret!",
+"failed!" => "feilet!",
+"Saved" => "Lagret",
+"There was a problem saving your changes. Click to resume editing." => "Det oppstod et problem ved lagring av endringene dine. Klikk for å fortsette redigering.",
+"An error occurred!" => "En feil oppstod!"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
