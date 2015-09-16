c56a944c04926884ba18afa2e1333abf11d3bf44
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-09-16 03:47:44 -0400

    [tx-robot] updated from transifex

--- l10n/sk_SK.js
+++ l10n/sk_SK.js
@@ -1,18 +1,16 @@
 OC.L10N.register(
     "files_texteditor",
     {
+    "Cannot read the file." : "Nemožno čítať súbor.",
     "Invalid file path supplied." : "Neplatná cesta k súboru.",
     "Cannot save file as it has been modified since opening" : "Nie je možné uložiť súbor, pretože bol od otvorenia zmenený",
     "Insufficient permissions" : "Nedostatočné oprávnenia",
     "File path not supplied" : "Nebola uvedená cesta k súboru",
     "File mtime not supplied" : "Nebol uvedený mtime súboru",
-    "Save" : "Uložiť",
-    "Search" : "Hľadať",
-    "Next" : "Ďalšia",
-    "Clear" : "Vyčistiť",
-    "Saving..." : "Ukladám...",
-    "An error occurred!" : "Vyskytla sa chyba!",
-    "There are unsaved changes in the text editor" : "V editore sú neuložené zmeny",
-    "There were unsaved changes, click here to go back" : "Zostali neuložené zmeny. Kliknite sem pre návrat"
+    "saving..." : "ukladám...",
+    "saved!" : "uložené!",
+    "failed!" : "neúspešné!",
+    "Saved" : "Uložené",
+    "An error occurred!" : "Vyskytla sa chyba!"
 },
 "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;");
