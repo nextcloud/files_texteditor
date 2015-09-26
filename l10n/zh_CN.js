5264ac3e44d1e5058ab459f4410a3048eecaafc2
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-09-26 02:11:17 -0400

    [tx-robot] updated from transifex

---
+++ l10n/zh_CN.js
@@ -0,0 +1,16 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "Cannot read the file." : "不能读取文件",
+    "Invalid file path supplied." : "提供了无效的文件路径",
+    "Insufficient permissions" : "权限不足",
+    "File path not supplied" : "未提供文件路径",
+    "File mtime not supplied" : "未提供mtime",
+    "saving..." : "保存中...",
+    "saved!" : "已保存",
+    "failed!" : "失败",
+    "Saved" : "已保存",
+    "There was a problem saving your changes. Click to resume editing." : "在保存时出现了一些问题，点击以恢复编辑",
+    "An error occurred!" : "发生错误！"
+},
+"nplurals=1; plural=0;");
--- l10n/zh_CN.php
+++ l10n/zh_CN.php
@@ -1,11 +1,15 @@
-<?php $TRANSLATIONS = array(
-"Save" => "保存",
-"Search" => "搜索",
-"Close" => "关闭",
-"Next" => "下一个",
-"Clear" => "清除",
-"Saving..." => "保存中",
-"Failed to save file" => "无法保存文件",
-"An error occurred!" => "发生错误！",
-"There were unsaved changes, click here to go back" => "有未保存的变更，点击回退"
+<?php
+$TRANSLATIONS = array(
+"Cannot read the file." => "不能读取文件",
+"Invalid file path supplied." => "提供了无效的文件路径",
+"Insufficient permissions" => "权限不足",
+"File path not supplied" => "未提供文件路径",
+"File mtime not supplied" => "未提供mtime",
+"saving..." => "保存中...",
+"saved!" => "已保存",
+"failed!" => "失败",
+"Saved" => "已保存",
+"There was a problem saving your changes. Click to resume editing." => "在保存时出现了一些问题，点击以恢复编辑",
+"An error occurred!" => "发生错误！"
 );
+$PLURAL_FORMS = "nplurals=1; plural=0;";
