6967626cbbb97f7bdd3d185babaf0eca80c38500
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-03 02:11:17 -0400

    [tx-robot] updated from transifex

--- l10n/fa.php
+++ l10n/fa.php
@@ -1,11 +1,16 @@
-<?php $TRANSLATIONS = array(
-"Save" => "ذخیره",
-"Search" => "جست‌و‌جو",
-"Close" => "بستن",
-"Next" => "بعدی",
-"Clear" => "معلوم",
-"Saving..." => "در حال ذخیره سازی...",
-"Failed to save file" => "برای ذخیره سازی در فایل انجام نشد",
-"An error occurred!" => "یک خطا رخ‌داده است!",
-"There were unsaved changes, click here to go back" => "There were unsaved changes, click here to go back"
+<?php
+$TRANSLATIONS = array(
+"Cannot read the file." => "امکان خواندن فایل وجود ندارد",
+"Invalid file path supplied." => "مسیر فایل نامعتبر است.",
+"Cannot save file as it has been modified since opening" => "با توجه به تغییرات رخ داده بر روی فایل از زمان باز شدن آن، امکان ذخیره‌سازی وجود ندارد.",
+"Insufficient permissions" => "دسترسی کافی نیست",
+"File path not supplied" => "مسیر فایل داده نشده است",
+"File mtime not supplied" => "mtime فایل داده نشده است",
+"saving..." => "درحال ذخیره",
+"saved!" => "ذخیره شد",
+"failed!" => "ناموفق!",
+"Saved" => "ذخیره شد",
+"There was a problem saving your changes. Click to resume editing." => "مشکلی در زمان ذخیره‌سازی تغییرات شما وجود داشت. برای ادامه ویرایش کلیک کنید.",
+"An error occurred!" => "یک خطا رخ‌داده است!"
 );
+$PLURAL_FORMS = "nplurals=1; plural=0;";
