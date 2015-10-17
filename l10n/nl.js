b21ef4a0e29c825de74ea17a8ea86c32568447a3
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-17 02:11:20 -0400

    [tx-robot] updated from transifex

--- l10n/nl.js
+++ l10n/nl.js
@@ -1,8 +1,10 @@
 OC.L10N.register(
     "files_texteditor",
     {
+    "This file is too big to be opened. Please download the file instead." : "Dit bestand is te groot om te worden geopend. Download het bestand om het te bewerken.",
     "Cannot read the file." : "Kan het bestand niet lezen.",
     "Invalid file path supplied." : "Ongeldig pad opgegeven.",
+    "An internal server error occurred." : "Er heeft zich een interne serverfout voorgedaan.",
     "Cannot save file as it has been modified since opening" : "Kan het bestand niet opslaan omdat het is gewijzigd na het openen",
     "Insufficient permissions" : "Onvoldoende rechten",
     "File path not supplied" : "Bestandspad niet opgegeven",
