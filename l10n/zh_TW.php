db71f6920a83ff0745b70853e899c59076ea2bed
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-24 02:11:24 -0400

    [tx-robot] updated from transifex

--- l10n/zh_TW.php
+++ l10n/zh_TW.php
@@ -1,11 +1,18 @@
-<?php $TRANSLATIONS = array(
-"Save" => "儲存",
-"Search" => "搜尋",
-"Close" => " 關閉",
-"Next" => "下一個",
-"Clear" => "清除",
-"Saving..." => "儲存中...",
-"Failed to save file" => "儲存檔案失敗",
-"An error occurred!" => "發生錯誤！",
-"There were unsaved changes, click here to go back" => "還有尚未儲存的變更，點這裡返回"
+<?php
+$TRANSLATIONS = array(
+"This file is too big to be opened. Please download the file instead." => "這個檔案太大以致於無法開啟，請您下載此檔案代替線上開啟",
+"Cannot read the file." => "無法讀取檔案。",
+"Invalid file path supplied." => "提供了無效的檔案路徑。",
+"An internal server error occurred." => "發生伺服器內部錯誤",
+"Cannot save file as it has been modified since opening" => "無法將變更內容儲存至檔案，檔案正被開啟中",
+"Insufficient permissions" => "權限不足",
+"File path not supplied" => "沒有提供檔案路徑",
+"File mtime not supplied" => "沒有提供檔案修改時間",
+"saving..." => "儲存中...",
+"saved!" => "已儲存!",
+"failed!" => "失敗!",
+"Saved" => "已儲存",
+"There was a problem saving your changes. Click to resume editing." => "在儲存您的變更時發生錯誤，點選以回復編輯狀態。",
+"An error occurred!" => "發生錯誤！"
 );
+$PLURAL_FORMS = "nplurals=1; plural=0;";
