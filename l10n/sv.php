8e19c689a1d61d18a0a454a590b8a4211c133f1f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-12-02 02:11:19 -0500

    [tx-robot] updated from transifex

--- l10n/sv.php
+++ l10n/sv.php
@@ -1,12 +1,20 @@
-<?php $TRANSLATIONS = array(
-"Save" => "Spara",
-"Search" => "Sök",
-"Close" => "Stäng",
-"Next" => "Nästa",
-"Clear" => "Rensa",
-"Saving..." => "Sparar...",
-"Failed to save file" => "Det gick inte att spara filen",
+<?php
+$TRANSLATIONS = array(
+"This file is too big to be opened. Please download the file instead." => "Denna fil är för stor för att öppnas. Var god ladda ner filen istället.",
+"Cannot read the file." => "Kan inte läsa av fil.",
+"Invalid file path supplied." => "Felaktig sökväg till fil angiven.",
+"An internal server error occurred." => "Ett server-fel har inträffat.",
+"Cannot save file as it has been modified since opening" => "Kan inte spara filen eftersom den har ändrats sedan den öppnats.",
+"Insufficient permissions" => "Otillräcklig behörighet.",
+"File path not supplied" => "Ingen sökväg till fil angiven.",
+"File mtime not supplied" => "Inget mtime-attribut angivet för filen.",
+"saving..." => "sparar...",
+"saved!" => "sparad!",
+"failed!" => "misslyckades.",
+"Saved" => "Sparad",
+"There was a problem saving your changes. Click to resume editing." => "Det uppstod ett fel när vi försökte spara dina ändringar. Klicka för att fortsätta redigera.",
 "An error occurred!" => "Ett fel uppstod!",
-"There are unsaved changes in the text editor" => "Det finns osparade ändringar i texteditorn",
-"There were unsaved changes, click here to go back" => "Det finns osparade ändringar, klicka här för att gå tillbaka"
+"Text file" => "Textfil",
+"New text file.txt" => "nytextfil.txt"
 );
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
