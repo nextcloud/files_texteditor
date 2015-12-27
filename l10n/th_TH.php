b4c3d4b4c529adf787dee9b8e3af56b03f785c59
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-12-27 02:14:05 -0500

    [tx-robot] updated from transifex

--- l10n/th_TH.php
+++ l10n/th_TH.php
@@ -3,6 +3,7 @@ $TRANSLATIONS = array(
 "This file is too big to be opened. Please download the file instead." => "เปิดไฟล์นี้ไม่ได้ เพราะมีขนาดใหญ่เกินไป กรุณาดาวน์โหลดไฟล์แทน",
 "Cannot read the file." => "ไม่สามารถอ่านไฟล์",
 "Invalid file path supplied." => "เส้นทางของไฟล์ที่ให้มาไม่ถูกต้อง",
+"The file is locked." => "ไฟล์ถูกล็อค",
 "An internal server error occurred." => "มีข้อผิดพลาดเกิดขึ้นภายในเซิร์ฟเวอร์",
 "Cannot save file as it has been modified since opening" => "ไม่สามารถบันทึกไฟล์ในขณะที่มันถูกเปิดและแก้ไขอยู่",
 "Insufficient permissions" => "สิทธิ์ไม่เพียงพอ",
