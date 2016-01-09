bdcad8ddc807c75a94fd679d6104287298f3146d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-01-09 02:11:38 -0500

    [tx-robot] updated from transifex

---
+++ l10n/he.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "This file is too big to be opened. Please download the file instead." : "קובץ זה גדול מדי לפתיחה. יש להוריד את הקובץ במקום",
+    "Cannot read the file." : "לא ניתן לקרוא את הקובץ.",
+    "Invalid file path supplied." : "סופק נתיב קובץ לא חוקי.",
+    "The file is locked." : "הקובץ נעול.",
+    "An internal server error occurred." : "אירעה שגיאת שרת פנימית.",
+    "Cannot save file as it has been modified since opening" : "לא ניתן לשמור את הקובץ כיוון שהקובץ שונה מאז פתיחתו",
+    "Insufficient permissions" : "אין הרשאות מספקות",
+    "File path not supplied" : "נתיב קובץ לא סופק",
+    "File mtime not supplied" : "קובץ mtime לא סופק",
+    "saving..." : "בשמירה...",
+    "saved!" : "נשמר!",
+    "failed!" : "נכשל!",
+    "Saved" : "נשמר",
+    "There was a problem saving your changes. Click to resume editing." : "אירעה בעייה בשמירת השינויים. יש ללחוץ לחידוש העריכה.",
+    "An error occurred!" : "אירעה תקלה!",
+    "Text file" : "קובץ טקסט",
+    "New text file.txt" : "קובץ טקסט חדש.txt"
+},
+"nplurals=2; plural=(n != 1);");
--- l10n/he.php
+++ l10n/he.php
@@ -1,11 +1,21 @@
-<?php $TRANSLATIONS = array(
-"Save" => "שמירה",
-"Search" => "חיפוש",
-"Close" => "סגירה",
-"Next" => "הבא",
-"Clear" => "ניקוי",
-"Saving..." => "שמירה…",
-"Failed to save file" => "נכשל בשמירת הקובץ",
+<?php
+$TRANSLATIONS = array(
+"This file is too big to be opened. Please download the file instead." => "קובץ זה גדול מדי לפתיחה. יש להוריד את הקובץ במקום",
+"Cannot read the file." => "לא ניתן לקרוא את הקובץ.",
+"Invalid file path supplied." => "סופק נתיב קובץ לא חוקי.",
+"The file is locked." => "הקובץ נעול.",
+"An internal server error occurred." => "אירעה שגיאת שרת פנימית.",
+"Cannot save file as it has been modified since opening" => "לא ניתן לשמור את הקובץ כיוון שהקובץ שונה מאז פתיחתו",
+"Insufficient permissions" => "אין הרשאות מספקות",
+"File path not supplied" => "נתיב קובץ לא סופק",
+"File mtime not supplied" => "קובץ mtime לא סופק",
+"saving..." => "בשמירה...",
+"saved!" => "נשמר!",
+"failed!" => "נכשל!",
+"Saved" => "נשמר",
+"There was a problem saving your changes. Click to resume editing." => "אירעה בעייה בשמירת השינויים. יש ללחוץ לחידוש העריכה.",
 "An error occurred!" => "אירעה תקלה!",
-"There were unsaved changes, click here to go back" => "ישנם שינויים שלא נשמרו. נא ללחוץ כאן לחזרה"
+"Text file" => "קובץ טקסט",
+"New text file.txt" => "קובץ טקסט חדש.txt"
 );
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
