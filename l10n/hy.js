5db35d521621098163a3dfd7594468aa7638a532
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-03-05 02:11:22 -0500

    [tx-robot] updated from transifex

---
+++ l10n/hy.js
@@ -0,0 +1,21 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "This file is too big to be opened. Please download the file instead." : "Ֆայլը չափից ավելի մեծ է բացելու համար։ Փոխարենը ներբեռնիր խնդրում եմ:",
+    "Cannot read the file." : "Չկարողացա կարդալ ֆայլը։",
+    "Invalid file path supplied." : "Անվավեր ֆայլի հասցե է տրամադրված։",
+    "The file is locked." : "Ֆայլը կողպած է։",
+    "An internal server error occurred." : "Սերվերի ներքին սխալ։",
+    "Cannot save file as it has been modified since opening" : "Չեմ կարող պահել ֆայլը, քանի որ այն բացելուց հետո փոփոխվել է",
+    "File path not supplied" : "Ֆայլի հասցեն տրամադրված չէ",
+    "File mtime not supplied" : "Ֆայլի mtimeը տրամադրված չէ",
+    "saving..." : "պահում եմ...",
+    "saved!" : "պահվե՛ց",
+    "failed!" : "ձախողվե՛ց",
+    "Saved" : "Պահված",
+    "There was a problem saving your changes. Click to resume editing." : "Խնդիր եղավ փոփոխությունները պահելիս։ Կտտացրու խմբագրումը շարունակելու համար։",
+    "An error occurred!" : "Սխալ պատահեց",
+    "Text file" : "Տեքստ ֆայլ",
+    "New text file.txt" : "Նոր տեքստ ֆայլ.txt"
+},
+"nplurals=2; plural=(n != 1);");
