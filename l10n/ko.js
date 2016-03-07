b7256960212495218637b5dd7b629143990c3aa9
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-03-07 02:11:22 -0500

    [tx-robot] updated from transifex

--- l10n/ko.js
+++ l10n/ko.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "This file is too big to be opened. Please download the file instead." : "이 파일을 열기에 너무 큽니다. 파일을 다운로드하십시오.",
     "Cannot read the file." : "파일을 읽을 수 없습니다.",
     "Invalid file path supplied." : "잘못된 경로를 지정했습니다.",
+    "The file is locked." : "이 파일이 잠겨 있습니다.",
     "An internal server error occurred." : "내부 서버 오류가 발생했습니다.",
     "Cannot save file as it has been modified since opening" : "파일을 연 후 수정했기 때문에 저장할 수 없음",
     "Insufficient permissions" : "권한 부족",
