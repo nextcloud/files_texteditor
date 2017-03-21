5a82ac187d20d839e385784b7c833d0c22d6b6b6
Lukas Reschke <lukas@statuscode.ch>
2017-03-21 16:42:06 +0100

    Use SCSS compiler to save requests
    Shaves off 3 CSS requests on page loads.
    
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- appinfo/app.php
+++ appinfo/app.php
@@ -4,12 +4,10 @@
 if (\OCP\User::isLoggedIn()) {
 	$eventDispatcher = \OC::$server->getEventDispatcher();
 	$eventDispatcher->addListener('OCA\Files::loadAdditionalScripts', function() {
-		OCP\Util::addStyle('files_texteditor', 'DroidSansMono/stylesheet');
-		OCP\Util::addStyle('files_texteditor', 'style');
-		OCP\Util::addStyle('files_texteditor', 'mobile');
-		OCP\Util::addscript('files_texteditor', 'editor');
-		OCP\Util::addscript('files_texteditor', 'sidebarpreview');
-		OCP\Util::addscript('files_texteditor', 'core/vendor/ace-builds/src-noconflict/ace');
+		OCP\Util::addStyle('files_texteditor', 'merged');
+		OCP\Util::addScript('files_texteditor', 'editor');
+		OCP\Util::addScript('files_texteditor', 'sidebarpreview');
+		OCP\Util::addScript('files_texteditor', 'core/vendor/ace-builds/src-noconflict/ace');
 
 		$cspManager = \OC::$server->getContentSecurityPolicyManager();
 		$csp = new \OCP\AppFramework\Http\ContentSecurityPolicy();
