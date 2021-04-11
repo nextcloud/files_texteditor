437a4cd3603617cd60eaf0be09862eaae5a91f97
Robbert Gurdeep Singh <git@beardhatcode.be>
2021-04-11 10:42:52 +0200

    fix: wrong translation string
    Remove dot at end of translation string to match PublicFileHandlingController and only have one translation string.
    
    Signed-off-by: Robbert Gurdeep Singh <git@beardhatcode.be>

--- lib/Controller/FileHandlingController.php
+++ lib/Controller/FileHandlingController.php
@@ -84,7 +84,7 @@ class FileHandlingController extends Controller {
 				$file = $this->userFolder->get($path);
 
 				if ($file instanceof Folder) {
-					return new DataResponse(['message' => $this->l->t('You cannot open a folder.')], Http::STATUS_BAD_REQUEST);
+					return new DataResponse(['message' => $this->l->t('You cannot open a folder')], Http::STATUS_BAD_REQUEST);
 				}
 
 				// default of 4MB
